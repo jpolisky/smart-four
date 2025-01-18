b08d552b9e445512850629d96ec2b42e717742ed
Jackson Polisky <poliskyjeff@gmail.com>
2025-01-17 22:50:54 -0500

    better organization!!!

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.172.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.172.0/examples/jsm/controls/OrbitControls.js';
-import { io } from 'https://cdn.jsdelivr.net/npm/socket.io-client@4.7.2/dist/socket.io.min.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { io } from 'socket.io-client';
 
 const socket = io('https://smartfour-server.onrender.com');
 socket.on('connect', () => {
